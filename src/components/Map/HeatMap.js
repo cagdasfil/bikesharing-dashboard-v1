b600f17a3e7c620e8ba48aee3b3e3c6f4e0e05d3
ABDULKADIR KILAVUZ <e2110260@ceng.metu.edu.tr>
2020-05-17 02:46:50 +0300

    fixing color issue on map

--- src/components/Map/HeatMap.js
+++ src/components/Map/HeatMap.js
@@ -84,7 +84,7 @@ class HeatMap extends PureComponent {
                     ["linear"],
                     ["heatmap-density"],
                     0,
-                    "rgb(33,102,172)",
+                    "rgba(33,102,172,0)",
                     0.2,
                     "rgb(103,169,207)",
                     0.4,
